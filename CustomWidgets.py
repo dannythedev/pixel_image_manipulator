65a2c78ee7a52abf0b4db9634353badcece01a8e
dannythedev <dannythedev@users.noreply.github.com>
2024-05-08 23:34:57 +0300

    Seperated files.

---
+++ CustomWidgets.py
@@ -0,0 +1,128 @@
+from tkinter.ttk import Progressbar
+import tkinter as tk
+
+
+class CustomMessageBox:
+    def __init__(self, title, message, link_text, link_command, bg_color="#121212", fg_color="#EEEEEE"):
+        self.root = tk.Tk()
+        self.root.withdraw()  # Hide the root window
+        self.title = title
+        self.message = message
+        self.link_text = link_text
+        self.link_command = link_command
+        self.bg_color = bg_color
+        self.fg_color = fg_color
+
+    def show(self):
+        top = tk.Toplevel(self.root)
+        top.title(self.title)
+        top.configure(bg=self.bg_color)
+
+        label = tk.Label(top, text=self.message, bg=self.bg_color, fg=self.fg_color)
+        label.pack(pady=10, padx=10)
+
+        link_label = tk.Label(top, text=self.link_text, fg="blue", cursor="hand2", bg=self.bg_color)
+        link_label.pack(pady=5, padx=10)
+        link_label.bind("<Button-1>", lambda event: self.link_command())
+
+        ok_button = tk.Button(top, text="Ok", command=top.destroy, bg="#007bff", fg="white", relief="flat", padx=10)
+        ok_button.pack(pady=10, padx=10)
+
+        top.mainloop()
+
+class CustomDialog:
+    def __init__(self, parent, title, prompt, width=210, height=80):
+        self.parent = parent
+        self.dialog = tk.Toplevel(parent)
+        self.dialog.title(title)
+
+        # Customize background color of the dialog
+        self.dialog.configure(bg="#121212")
+
+        self.label = tk.Label(self.dialog, text=prompt, bg="#121212", fg="white")
+        self.label.pack()
+
+        self.entry = tk.Entry(self.dialog)
+        self.entry.pack()
+
+        self.button = tk.Button(self.dialog, text="OK", command=self.on_ok, bg="#007bff", fg="white")
+        self.button.pack()
+
+        # Variable to store entered value
+        self.result = None
+
+        # Set window size and make it non-resizable
+        self.dialog.geometry(f"{width}x{height}")
+        self.dialog.resizable(False, False)
+
+    def on_ok(self):
+        # Store the entered value
+        self.result = self.entry.get()
+        self.dialog.destroy()
+
+    def show(self):
+        self.parent.wait_window(self.dialog)
+        return self.result
+
+
+class CustomProgressBar:
+    def __init__(self, parent):
+        self.progress_bar = Progressbar(parent, orient=tk.HORIZONTAL, mode='determinate')
+        self.progress_bar.grid(row=6, column=1)
+
+    def update_progress(self, value, maximum):
+        self.progress_bar['value'] = value
+        self.progress_bar['maximum'] = maximum
+
+    def destroy(self):
+        self.progress_bar.destroy()
+
+
+class ToolTip:
+    def __init__(self, widget, text, fade_in_period=0.5):
+        self.widget = widget
+        self.text = text
+        self.fade_in_period = fade_in_period
+        self.opacity = 0
+        self.tooltip = None
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
+
+    def enter(self, event=None):
+        self.create_tooltip()
+        self.reset()
+        self.tooltip.deiconify()
+
+    def reset(self):
+        self.opacity = 0
+        self.tooltip.attributes("-alpha", self.opacity)
+        self.widget.after(50, self.fade_in)
+
+    def leave(self, event=None):
+        self.hide_tooltip()
+
+    def create_tooltip(self):
+        x, y, _, _ = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 25
+        y += self.widget.winfo_rooty() + 25
+
+        self.tooltip = tk.Toplevel(self.widget)
+        self.tooltip.geometry(f"+{x}+{y}")
+        self.tooltip.overrideredirect(True)
+        self.tooltip.attributes("-alpha", self.opacity)
+
+        label = tk.Label(self.tooltip, text=self.text, bg="white", padx=5, pady=2)
+        label.pack()
+
+        self.fade_in()
+
+    def fade_in(self):
+        if self.opacity < 1:
+            self.opacity += 0.1
+            self.tooltip.attributes("-alpha", self.opacity)
+            self.widget.after(50, self.fade_in)
+        else:
+            self.opacity = 1
+
+    def hide_tooltip(self):
+        self.tooltip.withdraw()
---
+++ ImageManipulator.py
@@ -0,0 +1,236 @@
+import time
+from multiprocessing import Pool
+from tkinter import messagebox
+from PIL import Image, ImageDraw, ImageEnhance
+import numpy as np
+import os
+from collections import Counter
+from Functions import export_message
+
+class ImageManipulator:
+    @staticmethod
+    def resize_images(files, output_directory, resize_percent):
+        resized_directory = output_directory + "/resized_images"
+        os.makedirs(resized_directory, exist_ok=True)
+
+        for file in files:
+            img = Image.open(file)
+            width, height = img.size
+            new_width = int(width * resize_percent)
+            new_height = int(height * resize_percent)
+            resized_img = img.resize((new_width, new_height), Image.NEAREST)
+            file_name = os.path.basename(file)
+            resized_img.save(f"{resized_directory}/{file_name}")
+        export_message(files, message="Images resized and saved successfully.")
+
+    @staticmethod
+    def create_spritesheet(files, output_directory):
+        MAX_WIDTH = 3000
+        MAX_HEIGHT = 6000
+        PADDING = 5
+
+        current_x = 0
+        current_y = 0
+        max_row_height = 0
+        total_width = 0
+
+        for file in files:
+            img = Image.open(file)
+            width, height = img.size
+
+            if current_x + width > MAX_WIDTH:
+                current_x = 0
+                current_y += max_row_height + PADDING
+                max_row_height = 0
+
+            if current_y + height > MAX_HEIGHT:
+                messagebox.showwarning("Warning", "Spritesheet maximum size reached. Some images may be cut off.")
+                break
+
+            current_x += width + PADDING
+            max_row_height = max(max_row_height, height)
+            total_width = max(total_width, current_x)
+
+        spritesheet = Image.new("RGBA", (total_width, current_y + max_row_height), (255, 255, 255, 0))
+        draw = ImageDraw.Draw(spritesheet)
+
+        current_x = 0
+        current_y = 0
+        max_row_height = 0
+
+        for file in files:
+            img = Image.open(file)
+            width, height = img.size
+            aspect_ratio = width / height
+
+            if current_x + width > MAX_WIDTH:
+                current_x = 0
+                current_y += max_row_height + PADDING
+                max_row_height = 0
+
+            spritesheet.paste(img, (current_x, current_y))
+            draw.rectangle([current_x, current_y, current_x + width - 1, current_y + height - 1], outline="black")
+
+            current_x += width + PADDING
+            max_row_height = max(max_row_height, height)
+
+        spritesheet_path = output_directory + "/spritesheet.png"
+        spritesheet.save(spritesheet_path)
+        export_message(files, message="Spritesheet exported successfully.")
+
+    @staticmethod
+    def replace_color(files, output_directory, target_color, new_color):
+        replaced_directory = output_directory + "/replaced_images"
+        os.makedirs(replaced_directory, exist_ok=True)
+
+        for file in files:
+            img = Image.open(file)
+            img = img.convert("RGBA")
+            data = img.getdata()
+            newData = []
+            for item in data:
+                if item[:3] == target_color:
+                    newData.append(new_color + (item[3],))
+                else:
+                    newData.append(item)
+            img.putdata(newData)
+            file_name = os.path.basename(file)
+            img.save(f"{replaced_directory}/{file_name}")
+        export_message(files, message="Color replaced and images saved successfully.")
+
+    @staticmethod
+    def pixelate_image(image_path, block_size, saturation, remove_background, palette_name, palette,
+                       closest_color_cache=None):
+        image = Image.open(image_path)
+
+        pixelated_image, updated_cache = ImageManipulator.pixelate(image, block_size, palette_name, palette,
+                                                                   closest_color_cache=closest_color_cache)
+        pixelated_image = ImageManipulator.adjust_saturation(pixelated_image, saturation)
+        if remove_background:
+            pixelated_image = ImageManipulator.remove_img_background(pixelated_image)
+        output_dir = os.path.join(os.path.dirname(image_path), 'pixelated_images')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_path = os.path.join(output_dir, os.path.basename(image_path))
+        pixelated_image.save(output_path)
+
+        # Update the original closest_color_cache with the updated_cache
+        closest_color_cache.update(updated_cache)
+
+        return closest_color_cache  # Return the updated closest_color_cache dictionary
+
+    @staticmethod
+    def adjust_saturation(image, saturation):
+        if saturation == 0:
+            return image
+        image = image.convert("HSV")
+        data = np.array(image)
+        data[:, :, 1] = np.clip(data[:, :, 1] * (1 + saturation / 100), 0, 255)
+        return Image.fromarray(data, "HSV").convert("RGB")
+
+    @staticmethod
+    def adjust_contrast(image, contrast):
+        if contrast == 0:
+            return image
+
+        enhancer = ImageEnhance.Contrast(image)
+        # Convert contrast from a scale of -100 to 100 to a factor between 0 and 2
+        contrast_factor = 1.0 + contrast / 100.0
+        return enhancer.enhance(contrast_factor)
+
+    @staticmethod
+    def adjust_brightness(image, brightness):
+        if brightness == 0:
+            return image
+
+        enhancer = ImageEnhance.Brightness(image)
+        # Convert brightness from a scale of -100 to 100 to a factor between 0 and 2
+        brightness_factor = 1.0 + brightness / 100.0
+        return enhancer.enhance(brightness_factor)
+
+    @staticmethod
+    def process_block(args):
+        image, block_coords, palette_name, palette, closest_color_cache = args
+        x0, y0, x1, y1 = block_coords
+
+        region = np.array(image.crop((x0, y0, x1, y1)))
+        average_color = tuple(np.mean(region, axis=(0, 1)).astype(int))
+
+        if average_color in closest_color_cache[palette_name]:
+            closest = closest_color_cache[palette_name][average_color]
+            count = 1
+        else:
+            closest = ImageManipulator.closest_color(average_color, palette)
+            closest_color_cache[palette_name][average_color] = closest
+            count = 0
+
+        cache_update = {palette_name: {average_color: closest}}
+        return block_coords, closest, cache_update, (count, 1 - count)
+
+    @staticmethod
+    def pixelate(image, block_size, palette_name, palette, resize=True, closest_color_cache=None):
+        start = time.time()
+        width, height = image.size
+        h_blocks = height // block_size
+        w_blocks = width // block_size
+        new_image = image.copy()
+
+        block_coords = [(i * block_size, j * block_size, (i + 1) * block_size, (j + 1) * block_size)
+                        for j in range(h_blocks) for i in range(w_blocks)]
+
+        palette_cache_updates = {}  # Accumulate updates for each palette separately
+        count1, count2 = 0, 0
+        for coords in block_coords:
+            args = (image, coords, palette_name, palette, closest_color_cache)
+            block_coords, closest, cache_update, counts = ImageManipulator.process_block(args)
+            count1 += counts[0]
+            count2 += counts[1]
+            new_image.paste(closest, block_coords)
+
+            # Accumulate updates for each palette
+            if palette_name not in palette_cache_updates:
+                palette_cache_updates[palette_name] = {}
+            palette_cache_updates[palette_name].update(cache_update)
+        print(f'{count1}/{count1+count2}')
+        # Update the cache for each palette after processing all blocks
+        if count2 != 0:
+            for palette_name, updates in palette_cache_updates.items():
+                closest_color_cache[palette_name].update(updates)
+
+        print(abs(start - time.time()))
+        return new_image, closest_color_cache
+
+    @staticmethod
+    def convert(photoimage):
+        return Image(photoimage)
+
+    @staticmethod
+    def closest_color(pixel, palette):
+        min_dist = float('inf')
+        closest = None
+        for color in palette:
+            dist = np.linalg.norm(np.array(pixel[:3]) - np.array(color))
+            if dist < min_dist:
+                min_dist = dist
+                closest = color
+        return closest
+
+    @staticmethod
+    def remove_img_background(image):
+        image = image.convert("RGBA")
+        pixels = image.getdata()
+        color_counts = Counter(pixels)
+        max_color = color_counts.most_common(1)[0][0]
+        return ImageManipulator.remove_color(image, max_color)
+
+    @staticmethod
+    def remove_color(image, color):
+        newData = []
+        pixels = image.getdata()
+        for item in pixels:
+            if item[:3] == color[:3]:
+                newData.append((color[0], color[1], color[2], 0))
+            else:
+                newData.append(item)
+        image.putdata(newData)
+        return image
